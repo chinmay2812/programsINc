0538147d60d9abd632349e8a63034b5d96d2bf40
chinmay2812 <83934925+chinmay2812@users.noreply.github.com>
2021-07-18 22:44:01 +0530

    Add files via upload

---
+++ LAB9/P3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    FILE *fp;
+    char data[100];
+    fp = fopen("BMSCE.txt","w");
+    if(fp==NULL)
+    {
+        printf("\nFile could not be opened");
+        exit(1);
+    }
+    printf("Enter data : ");
+    gets(data);
+    fprintf(fp,"%s",data);
+
+    fscanf(fp,"%s",data);
+    printf("Data from file : \n");
+    printf("%s\n",data);
+    fclose(fp);
+}
